586b2392f337e914a2b8f50b89461d7e1b640305
Shadow pryo <terry.dewayne.hicks@gmail.com>
2019-06-30 21:25:04 -0400

    Added playercharacter and movment script, basic but will work on as we go.

---
+++ Assets/Scripts/Sword.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    public int STRMod;
+    public bool isEquipped = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isEquipped)
+        {
+            SphereCollider spherCollider = GetComponent<SphereCollider>();
+            Destroy(spherCollider);
+        }
+    }
+}
